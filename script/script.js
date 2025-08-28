50df75816be71acc849f822258876b762eefa116
Rafiul Islam Jisan <rafiuljisan2005@gmail.com>
2025-08-28 18:02:32 +0600

    added heart clicking count and calling alert and history function

--- script/script.js
+++ script/script.js
@@ -1,40 +1,48 @@
 // heart click setup 
-function increaseCountValue(id) {
-    const heartCountV = parseInt(document.getElementById("heart-count").innerText);
-    const newCountV = heartCountV + 1;
-    document.getElementById('heart-count').innerText = newCountV;
-}
-document.getElementById('heart-1').addEventListener('click', function () {
-increaseCountValue('heart-1')
-})
-document.getElementById('heart-2').addEventListener('click', function () {
-increaseCountValue('heart-2')
-})
-document.getElementById('heart-3').addEventListener('click', function () {
-increaseCountValue('heart-3')
-})
-document.getElementById('heart-4').addEventListener('click', function () {
-increaseCountValue('heart-4')
-})
-document.getElementById('heart-5').addEventListener('click', function () {
-increaseCountValue('heart-5')
-})
-document.getElementById('heart-6').addEventListener('click', function () {
-increaseCountValue('heart-6')
-})
-document.getElementById('heart-7').addEventListener('click', function () {
-increaseCountValue('heart-7')
-})
-document.getElementById('heart-8').addEventListener('click', function () {
-increaseCountValue('heart-8')
-})
-document.getElementById('heart-9').addEventListener('click', function () {
-increaseCountValue('heart-9')
+document.getElementById('card-container').addEventListener('click', function (event) {
+    if (event.target.classList.contains('heart')) {
+        const heartCountV = parseInt(document.getElementById("heart-count").innerText);
+        const newCountV = heartCountV + 1;
+        document.getElementById('heart-count').innerText = newCountV;
+    }
 })
 
+// call operation setup 
+document.getElementById('card-container').addEventListener('click', function (event) {
+    if (event.target.classList.contains('callBtn')) {
 
 
+        const remainCoin = document.getElementById('coin').innerText;
+        // insufficient alert
+        if (remainCoin < 20) {
+            alert('Insufficient Coin');
+            return
+        }
+        // decreasing coin
+        document.getElementById('coin').innerText = remainCoin - 20;
+
+        // calling alert
+        const nearestCard = event.target.closest('.card');
+        const name = nearestCard.querySelector('.div .name').innerText;
+        console.log(name)
+        const num = nearestCard.querySelector('.div .num').innerText;
+        alert("Calling " + name + ": " + num)
+
+        // creating a new div
+        const historyDiv = document.createElement('div');
+        historyDiv.className = "flex justify-between items-center bg-gray-50 mx-6 my-3 p-5 rounded-2xl";
+        historyDiv.innerHTML =
+            `<div>
+            <h4 class="font-semibold text-xl">${name}</h4>
+            <p class="text-gray-500">${num}</p>
+        </div>
+        <div>
+            <p class="text-gray-500">${new Date().toLocaleTimeString()} </p>
+        </div>`
+
+        // appending the new div to the parent div   
+        document.getElementById('history').appendChild(historyDiv);
 
 
-
-
+    }
+})
